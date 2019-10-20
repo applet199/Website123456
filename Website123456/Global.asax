f9fa5f400c5c2f9c72cf3e38955a83f69b62b92e
applet199 <applet199@gmail.com>
2019-10-20 10:10:57 +0100

    Add project files.

---
+++ Website123456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Website123456.MvcApplication" Language="C#" %>
---
+++ Website123456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Website123456.Startup))]
+namespace Website123456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
